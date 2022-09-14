d6b2653ed37c25fb9e04c0b303f5c156307c5786
DJangoo00 <dalgramdo@gmail.com>
2022-09-13 21:13:17 -0500

    cambio de interfaz administrador e inclusion de ingresar usuario pendiente terminar

---
+++ mascota/model/admin/add_usuario.php
@@ -0,0 +1,61 @@
+
+<?php
+session_start();
+require_once("../../db/connection.php");
+include("../../controller/validarSesion.php");
+$sql = "SELECT * FROM user, tip_user WHERE user = '".$_SESSION['usuario']."' AND user.id_tip_user = tip_user.id_tip_user";
+$usuarios = mysqli_query($mysqli, $sql);
+$usua = mysqli_fetch_assoc($usuarios);
+
+
+?>
+<form method="POST">
+
+    <tr>
+        <td colspan='2' align="center"><?php echo $usua['nombres']?></td>
+    </tr>
+<tr><br>
+    <td colspan='2' align="center">
+    
+    
+        <input type="submit" value="Cerrar sesión" name="btncerrar" /></td>
+        <input type="submit" formaction="../index.php" value="Regresar" />
+    </tr>
+</form>
+
+<?php 
+
+if(isset($_POST['btncerrar']))
+{
+	session_destroy();
+
+   
+    header('location: ../../index.html');
+}
+	
+?>
+
+</div>
+
+</div>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
+    <title>Agregar Usuario</title>
+</head>
+    <body>
+        <section class="title">
+            <h1><?php echo $usua['tip_user']?> | Formulario para agregar tipo de usuario</h1>
+        </section>
+        <table>    
+
+    </body>
+</html>
